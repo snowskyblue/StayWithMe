76c7a1e0b71c91291be7852b3b1de42ebff6e3c0
김가영 <rlarkddud42@gmail.com>
2019-12-27 17:48:25 +0900

    변경

---
+++ src/main/java/com/jsk/stay/dto/Accommodation.java
@@ -0,0 +1,12 @@
+package com.jsk.stay.dto;
+
+import java.sql.Timestamp;
+
+public class Accommodation {
+	private int acm_code;
+	private String acm_img;
+	private String acm_info;
+	private String acm_rule;
+	private int acm_charge;
+	private Timestamp acm_availdate;
+}
