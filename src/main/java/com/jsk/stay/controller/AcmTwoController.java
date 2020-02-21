9a29ca688b24fd7a7f1c441518d1a43e3b8687f9
Seonghyun Kim <juliakim0823@gmail.com>
2020-02-21 09:52:26 +0900

    위시리스트 ck에디터 에러 보완

--- src/main/java/com/jsk/stay/controller/AcmTwoController.java
+++ src/main/java/com/jsk/stay/controller/AcmTwoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jsk.stay.command.AcmTwoCommand;
 import com.jsk.stay.dto.AccommodationDto;
+import com.jsk.stay.dto.AccommodationDto2;
 import com.jsk.stay.dto.CriteriaAcm;
 import com.jsk.stay.dto.PageMakerAcm;
 
@@ -38,9 +39,33 @@ public class AcmTwoController {
 		
 		model.addAttribute("pageMaker", pageMaker);
 		
-
+		List<AccommodationDto2> acmList2 = new ArrayList<AccommodationDto2>();
+		
+		for(AccommodationDto dto1 : acmList) {
+			AccommodationDto2 dto2 = new AccommodationDto2();
+			dto2.setMb_id(dto1.getMb_id());
+			dto2.setAcm_code(dto1.getAcm_code());
+			dto2.setAcm_charge(dto1.getAcm_charge());
+			dto2.setAcm_availdate(dto1.getAcm_availdate());
+			dto2.setAcm_checkin_time(dto1.getAcm_checkin_time());
+			dto2.setAcm_checkout_time(dto1.getAcm_checkout_time());
+			dto2.setAcm_type(dto1.getAcm_type());
+			dto2.setAcm_room_type(dto1.getAcm_room_type());
+			dto2.setAcm_guest_num(dto1.getAcm_guest_num());
+			dto2.setAcm_room_num(dto1.getAcm_room_num());
+			dto2.setAcm_bath_num(dto1.getAcm_bath_num());
+			dto2.setAcm_area(dto1.getAcm_area());
+			dto2.setAcm_bedding(dto1.getAcm_bedding());
+			dto2.setAcm_address(dto1.getAcm_address());
+			dto2.setAcm_add_detail(dto1.getAcm_add_detail());
+			dto2.setAcm_zip(dto1.getAcm_zip());
+			dto2.setAcm_title(dto1.getAcm_title());
+			dto2.setAcm_thumbnail(dto1.getAcm_thumbnail());
+			acmList2.add(dto2);
+		}
+		
 		JSONArray jsonArray = new JSONArray();
-		model.addAttribute("jsonList", jsonArray.fromObject(acmList));
+		model.addAttribute("jsonList", jsonArray.fromObject(acmList2));
 		return "acmList";
 	}
 	
